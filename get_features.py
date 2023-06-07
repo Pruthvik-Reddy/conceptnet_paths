49101a8aac7b6d412fd38bd163a7f69e691c4ab1
Pruthvik-Reddy <pruthvikreddy.21@gmail.com>
2023-06-07 17:19:18 -0400

    saving features to excel file get_features.py

--- get_features.py
+++ get_features.py
@@ -40,12 +40,50 @@ for filename in os.listdir(literal_path):
                             relation_count[word] += 1
                         else:
                             relation_count[word] = 1
+            relation_count["metaphor"]=0
+            file_name=file_name.replace(".txt","")
+            segments=file_name.split("_")
+            word1 = segments[1].split("-")[0]
+            word2 = segments[1].split("-")[1]
+            relation_count["verb1"]=word1
+            relation_count["verb2"]=word2
             new_row = {col: [relation_count.get(col, 0)] for col in df.columns}
             df.loc[len(df)]=new_row
             
-print(df)
 
 for filename in os.listdir(metaphor_path):
     file_path = os.path.join(metaphor_path, filename)
     if os.path.isfile(file_path):
         print(file_path)
+        with open(file_path, 'r') as file:
+            lines = file.readlines()
+
+            X = []
+            relation_count = dict()
+            for line in lines:
+                line = line.strip()
+                if line:
+                    segments = line.split(']')
+                    extracted_words = []
+                    for segment in segments:
+                        if '[' in segment:
+                            words = segment.split('[')[-1]
+                            extracted_words.extend([word.strip().strip("'") for word in words.split(',')])
+                    for word in extracted_words:
+                        if word in relation_count:
+                            relation_count[word] += 1
+                        else:
+                            relation_count[word] = 1
+            relation_count["metaphor"]=1
+            file_name=file_name.replace(".txt","")
+            segments=file_name.split("_")
+            word1 = segments[1].split("-")[0]
+            word2 = segments[1].split("-")[1]
+            relation_count["verb1"]=word1
+            relation_count["verb2"]=word2
+            new_row = {col: [relation_count.get(col, 0)] for col in df.columns}
+            df.loc[len(df)]=new_row
+
+print("The length of Data Frame is : ",len(df))
+df.to_excel("features.xlsx",index=False)
+    
