d61ad28ebd8b4acc42a3f8c4218a1b1bcca2a30a
Pruthvik-Reddy <pruthvikreddy.21@gmail.com>
2023-06-07 16:45:46 -0400

    solved error in get_features.py

--- get_features.py
+++ get_features.py
@@ -2,52 +2,49 @@ import pandas as pd
 import csv
 import os
 
-
-columns = ['verb1','verb2','Antonym', 'DistinctFrom', 'EtymologicallyRelatedTo', 'LocatedNear', 'RelatedTo',
-           'SimilarTo', 'Synonym', 'AtLocation', 'CapableOf', 'Causes', 'CausesDesire', 'CreatedBy',
-           'DefinedAs', 'DerivedFrom', 'Desires', 'Entails', 'ExternalURL', 'FormOf', 'HasA',
-           'HasContext', 'HasFirstSubevent', 'HasLastSubevent', 'HasPrerequisite', 'HasProperty',
-           'InstanceOf', 'IsA', 'MadeOf', 'MannerOf', 'MotivatedByGoal', 'ObstructedBy', 'PartOf',
-           'ReceivesAction', 'SenseOf', 'SymbolOf', 'UsedFor',"metaphor"]
-
+columns = [
+    'verb1', 'verb2', 'Antonym', 'DistinctFrom', 'EtymologicallyRelatedTo', 'LocatedNear', 'RelatedTo',
+    'SimilarTo', 'Synonym', 'AtLocation', 'CapableOf', 'Causes', 'CausesDesire', 'CreatedBy',
+    'DefinedAs', 'DerivedFrom', 'Desires', 'Entails', 'ExternalURL', 'FormOf', 'HasA',
+    'HasContext', 'HasFirstSubevent', 'HasLastSubevent', 'HasPrerequisite', 'HasProperty',
+    'InstanceOf', 'IsA', 'MadeOf', 'MannerOf', 'MotivatedByGoal', 'ObstructedBy', 'PartOf',
+    'ReceivesAction', 'SenseOf', 'SymbolOf', 'UsedFor', "metaphor"
+]
 
 df = pd.DataFrame(columns=columns)
 df.columns = df.columns.str.lower()
 
-literal_path="./conceptnet_paths/literal_pair_paths/"
-metaphor_path="./conceptnet_paths/metaphoric_pair_paths/"
+literal_path = "./conceptnet_paths/literal_pair_paths/"
+metaphor_path = "./conceptnet_paths/metaphoric_pair_paths/"
 
 for filename in os.listdir(literal_path):
     file_path = os.path.join(literal_path, filename)
     if os.path.isfile(file_path):
-        
         print(file_path)
         with open(file_path, 'r') as file:
             lines = file.readlines()
 
             X = []
-            relation_count=dict()
+            relation_count = dict()
             for line in lines:
                 line = line.strip()
                 if line:
-                    segments = string.split(']')
-		    extracted_words = []
-		    for segment in segments:
-   	                if '[' in segment:
-        		    words = segment.split('[')[-1]
-        		    extracted_words.extend([word.strip().strip("'") for word in words.split(',')])
-                for word in extracted_words:
-                    if word in relation_count: 
-                        relation_count[word]+=1
-                    else :
-                        relation_count[word]=1
+                    segments = line.split(']')
+                    extracted_words = []
+                    for segment in segments:
+                        if '[' in segment:
+                            words = segment.split('[')[-1]
+                            extracted_words.extend([word.strip().strip("'") for word in words.split(',')])
+                    for word in extracted_words:
+                        if word in relation_count:
+                            relation_count[word] += 1
+                        else:
+                            relation_count[word] = 1
             new_row = {col: [relation_count.get(col, 0)] for col in df.columns}
-            df.append(new_row,ignore_index=True)
+            df.append(new_row, ignore_index=True)
 print(df)
+
 for filename in os.listdir(metaphor_path):
     file_path = os.path.join(metaphor_path, filename)
     if os.path.isfile(file_path):
-        
         print(file_path)
- 
-
