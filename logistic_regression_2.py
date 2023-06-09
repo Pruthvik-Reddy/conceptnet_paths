06f4f5cfbaac0101b7a4b8c99a35dc95ca248c7e
Pruthvik-Reddy <pruthvikreddy.21@gmail.com>
2023-06-09 00:46:19 -0400

    Running logistic regression for other features

---
+++ logistic_regression_2.py
@@ -0,0 +1,70 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+
+columns = [
+     'Antonym', 'DistinctFrom', 'EtymologicallyRelatedTo', 'LocatedNear', 'RelatedTo',
+    'SimilarTo', 'Synonym', 'AtLocation', 'CapableOf', 'Causes', 'CausesDesire', 'CreatedBy',
+    'DefinedAs', 'DerivedFrom', 'Desires', 'Entails', 'ExternalURL', 'FormOf', 'HasA',
+    'HasContext', 'HasFirstSubevent', 'HasLastSubevent', 'HasPrerequisite', 'HasProperty',
+    'InstanceOf', 'IsA', 'MadeOf', 'MannerOf', 'MotivatedByGoal', 'ObstructedBy', 'PartOf',
+    'ReceivesAction', 'SenseOf', 'SymbolOf', 'UsedFor'
+]
+
+data=pd.read_excel("features.xlsx")
+data2=pd.read_excel("features.xlsx")
+for col in columns:
+    data[col]=data[col].str.replace('[', '').str.replace(']', '').astype(int)
+    data2[col]=data2[col].str.replace('[', '').str.replace(']', '').astype(int)
+
+for index, row in data2[columns].iterrows():
+    for col in columns:
+        if row[col] > 0:
+            data2.at[index, col] = 1
+        else:
+            data2.at[index, col] = 0
+X = data[columns]
+y = data['metaphor']
+
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+
+model = LogisticRegression()
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+
+accuracy = accuracy_score(y_test, y_pred)
+precision = precision_score(y_test, y_pred)
+recall = recall_score(y_test, y_pred)
+f1 = f1_score(y_test, y_pred)
+
+print('Accuracy:', accuracy)
+print('Precision:', precision)
+print('Recall:', recall)
+print('F1 Score:', f1)
+
+X = data2[columns]
+y = data2['metaphor']
+
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+
+model = LogisticRegression()
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+
+accuracy = accuracy_score(y_test, y_pred)
+precision = precision_score(y_test, y_pred)
+recall = recall_score(y_test, y_pred)
+f1 = f1_score(y_test, y_pred)
+
+print('Accuracy:', accuracy)
+print('Precision:', precision)
+print('Recall:', recall)
+print('F1 Score:', f1)
+
