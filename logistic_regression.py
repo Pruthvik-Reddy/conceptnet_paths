e94a9e2d8426c256dab54ecaf661b6370c3b6d51
Pruthvik-Reddy <pruthvikreddy.21@gmail.com>
2023-06-09 14:25:13 -0400

    Added the code to get Importance of Features in logistic_regression.py

--- logistic_regression.py
+++ logistic_regression.py
@@ -2,6 +2,10 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+import numpy as np
+from sklearn.inspection import permutation_importance
+
+
 
 column_df=pd.read_excel("all_unique_paths_2.xlsx")
 column_list = column_df[0].tolist()
@@ -28,3 +32,17 @@ print('Accuracy:', accuracy)
 print('Precision:', precision)
 print('Recall:', recall)
 print('F1 Score:', f1)
+
+
+print();print()
+print("Printing the Importance of Features :")
+print(model.coef_)
+
+# Those values, however, will show that the second parameter
+# is more influential
+print(np.std(X, 0)*model.coef_)
+
+print()
+print("Model Inspection :")
+model_fi = permutation_importance(model, X, y)
+print(model_fi['importances_mean'])
