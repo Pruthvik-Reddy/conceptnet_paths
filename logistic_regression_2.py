13c7e4235828dd5f466354eb37c774132f068a94
Pruthvik-Reddy <pruthvikreddy.21@gmail.com>
2023-06-09 00:55:47 -0400

    Running logistic regression for other features

--- logistic_regression_2.py
+++ logistic_regression_2.py
@@ -36,7 +36,7 @@ y = data['metaphor']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 
-model = LogisticRegression()
+model = LogisticRegression(max_iter=10000,solver="sag")
 model.fit(X_train, y_train)
 
 y_pred = model.predict(X_test)
@@ -58,7 +58,7 @@ y = data2['metaphor']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 
-model = LogisticRegression()
+model = LogisticRegression(max_iter=10000,solver="sag")
 model.fit(X_train, y_train)
 
 y_pred = model.predict(X_test)
